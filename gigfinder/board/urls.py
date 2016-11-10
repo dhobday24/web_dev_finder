b383ad18ce4e5636e3a58e14efb157592f28d087
bytesizedroll <damienhobday@gmail.com>
2016-11-10 16:25:13 -0500

    ID based selection works for events.

--- gigfinder/board/urls.py
+++ gigfinder/board/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.board, name = 'board'),
-    url(r'^events/', views.events, name = 'events'),
-    url(r'^job_posts/', views.job_posts, name = 'job_posts'),
-    url(r'^talent_ads/', views.musician_ads, name = 'musician_ads'),
-    url(r'^events/?P<event_id>[0-9]+/$', views.long_description_event, name = 'long_description_event')
+    url(r'^events/$', views.events, name = 'events'),
+    url(r'^job_posts/$', views.job_posts, name = 'job_posts'),
+    url(r'^talent_ads/$', views.musician_ads, name = 'musician_ads'),
+    url(r'^events/(?P<event_id>[0-9]+)/$', views.long_description_event, name = 'long_description_event')
 
 ]
--- gigfinder/board/views.py
+++ gigfinder/board/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Event, Job_Posting, Musician_Advertisement
