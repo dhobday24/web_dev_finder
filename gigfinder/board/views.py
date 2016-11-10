125ee3856efd50d516a164f982530d95b65ae943
bytesizedroll <damienhobday@gmail.com>
2016-11-09 22:59:32 -0500

    Changed views to user render() instead of HttpResponse().

--- gigfinder/board/views.py
+++ gigfinder/board/views.py
@@ -6,24 +6,21 @@ from .models import Event, Job_Posting, Musician_Advertisement
 
 def events(request):
     latest_events_list = Event.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('board/event_board.html')
     context = {
         'latest_events_list': latest_events_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'board/event_board.html', context)
 
 def job_posts(request):
     latest_posts_list = Job_Posting.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('board/job_posts_board.html')
     context = {
         'latest_posts_list': latest_posts_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'board/job_posts_board.html', context)
 
 def musician_ads(request):
     latest_ads_list = Musician_Advertisement.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('board/musician_ads_board.html')
     context = {
         'latest_ads_list': latest_ads_list,
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'board/musician_ads_board.html', context)
