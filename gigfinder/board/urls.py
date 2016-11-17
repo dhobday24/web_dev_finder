8691fcb40038d26c941e18513e842d7a43a15f0f
bytesizedroll <damienhobday@gmail.com>
2016-11-17 15:45:00 -0500

    Forms to new pages.

--- gigfinder/board/urls.py
+++ gigfinder/board/urls.py
@@ -8,6 +8,9 @@ urlpatterns = [
     url(r'^talent_ads/$', views.musician_ads, name = 'musician_ads'),
     url(r'^events/(?P<event_id>[0-9]+)/$', views.long_description_event, name = 'long_description_event'),
     url(r'^job_posts/(?P<job_id>[0-9]+)/$', views.long_description_job, name = 'long_description_job'),
-    url(r'^talent_ads/(?P<ad_id>[0-9]+)/$', views.long_description_musad, name = 'long_description_musad')
+    url(r'^talent_ads/(?P<ad_id>[0-9]+)/$', views.long_description_musad, name = 'long_description_musad'),
+    url(r'^event_submit/$', views.event_submit, name = 'event_submit'),
+    url(r'^job_submit/$', views.job_submit, name = 'job_submit'),
+    url(r'^ad_submit/$', views.ad_submit, name = 'ad_submit'),
 
 ]
