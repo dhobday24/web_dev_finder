4ed1366e7855fb817b5d688b7ec6fdfccd8906ac
Saroosh-z <sarooshzaman@gmail.com>
2016-11-21 22:08:49 -0500

    deleted duplicate folder 'homePage/template/homePage'.. now templates are in homePage/template. modified homePage/views.py accordingly"

--- gigfinder/homePage/views.py
+++ gigfinder/homePage/views.py
@@ -5,8 +5,7 @@ from django.http import HttpResponse
 from django.template import loader
 # Create your views here.
 def index(request):   
-   
-    return render(request, 'homePage/index.html')
+    return render(request, 'index.html')
 
 
 
