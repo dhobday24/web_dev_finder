d73b8bdcf01870bb64d99a55edd106f302d8556f
Connie Liu <heyconnie123@gmail.com>
2016-11-22 14:01:47 -0500

    url edits

--- gigfinder/authentication/urls.py
+++ gigfinder/authentication/urls.py
@@ -3,10 +3,7 @@ from django.contrib import admin
 from authentication import views
 
 urlpatterns = [
-    url(r'^board/', include('board.urls')),
-    url(r'^admin/', admin.site.urls),
     url(r'^$', views.home),
-    url(r'^home/$', views.home),
     url(r'^register/$', views.register),
     url(r'^register/success/$', views.register_success),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login'),
