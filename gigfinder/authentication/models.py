dba2321eb2543357d80b04d68c7c9ddc19406210
bytesizedroll <damienhobday@gmail.com>
2016-12-01 12:42:28 -0500

    Wut iz userz?

--- gigfinder/authentication/models.py
+++ gigfinder/authentication/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class UserProfile(models.Model):
+#class UserProfile(models.Model):
     # This line is required. Links UserProfile to a User model instance.
-    user = models.OneToOneField(User)
+#    user = models.OneToOneField(User)
 
     # Override the __unicode__() method to return out something meaningful!
-    def __unicode__(self):
-        return self.user.username
+#    def __unicode__(self):
+#        return self.user.username
--- gigfinder/authentication/views.py
+++ gigfinder/authentication/views.py
@@ -22,7 +22,7 @@ def register(request):
             user = User.objects.create_user(
                 username=form.cleaned_data['username'],
                 password=form.cleaned_data['password1'],
-                email=form.cleaned_data['email']
+                email=form.cleaned_data['email'],
             )
             return HttpResponseRedirect('/register/success/')
     else:
