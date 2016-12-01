8a0ebd5ccd7531c107c37c4a6b84051f04b49647
Abhishek Samdaria <samdaria@bxscience.edu>
2016-11-30 19:48:06 -0500

    run nose only on authentication and board app

--- gigfinder/gigfinder/settings.py
+++ gigfinder/gigfinder/settings.py
@@ -114,6 +114,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 
 NOSE_ARGS = [
     '--with-coverage',
+    '--cover-package=authentication,board',
     ]
 
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
