086e9783964d24671f1f35872f94e8ee0febfbc8
bytesizedroll <damienhobday@gmail.com>
2016-12-01 23:45:38 -0500

    Back to accounts/login.

--- gigfinder/authentication/urls.py
+++ gigfinder/authentication/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^register/$', views.register, name='register'),
     url(r'^register/success/$', views.register_success, name='register_success'),
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^/accounts/login/$', auth_views.login, name='login'),
     url(r'^logout/$', views.logout_page, name='logout_page'),
     url(r'^home/$', views.home, name='home'),
 ]
