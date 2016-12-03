89ad8a953d8f5bde4ac47c53672efdc5d318361b
Abhishek Samdaria <samdaria@bxscience.edu>
2016-12-03 02:02:25 -0500

    Refactored test code in board app

---
+++ gigfinder/board/tests/test_forms.py
@@ -0,0 +1,16 @@
+from django.test import TestCase
+from board.forms import *
+import unittest
+from board import models
+import datetime
+from django.utils import timezone
+
+class gigfinderTests(TestCase):
+
+	def test_forms(self):
+		event_form = EventForm()
+		job_form = JobForm()
+		ad_form = AdForm()
+		self.assertFalse(event_form.is_valid())
+		self.assertFalse(job_form.is_valid())
+		self.assertFalse(ad_form.is_valid())
