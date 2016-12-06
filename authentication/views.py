201aa25ef1a076d6dad872a40298a4ec7389f0d5
Abhishek Samdaria <samdaria@bxscience.edu>
2016-12-06 13:54:31 -0500

    Text the applicant who has been accepted

--- authentication/views.py
+++ authentication/views.py
@@ -14,6 +14,7 @@ from django.contrib.auth.models import User
 from django.forms.models import inlineformset_factory
 from django.core.exceptions import PermissionDenied
 
+import requests
 from authentication.models import UserProfile
 from authentication.forms import UserForm, RegistrationForm
 from address.models import Address
@@ -92,6 +93,7 @@ def home(request):
     # print(request.user.userprofile.address)
     return render_to_response('home.html', {'user': request.user, 'pk' : request.user.id, 'profile_pic': profile_pic, 'address': address, 'soundcloud_username': soundcloud_username})
 
+
 @login_required() # only logged in users should access this
 def edit_user(request, pk):
     """
@@ -137,11 +139,13 @@ def edit_user(request, pk):
     else:
         raise PermissionDenied
 
+
 def update_success(request):
     """
     Render the registration success page
     """
     return render_to_response('registration/update_success.html')
+
 
 def get_user_profile(request, username):
     current_user = request.user
@@ -150,6 +154,7 @@ def get_user_profile(request, username):
     soundcloud_username = user.userprofile.soundcloud_username
     profile_pic = user.userprofile.profile_pic
     return render(request, 'user_profile.html', {"user":user, 'profile_pic': profile_pic, "current_user":current_user, 'soundcloud_username': soundcloud_username})
+
 
 def my_events(request):
     current_user = request.user
@@ -160,6 +165,7 @@ def my_events(request):
     }
     return render(request, 'my_events.html', context)
 
+
 def my_ads(request):
     current_user = request.user
     all_ads = Musician_Advertisement.objects.all()
@@ -168,6 +174,7 @@ def my_ads(request):
         'current_user': current_user,
     }
     return render(request, 'my_ads.html', context)
+
 
 def show_applicants_event(request, event_id):
     event = Event.objects.filter(id = event_id).get()
@@ -179,14 +186,23 @@ def show_applicants_event(request, event_id):
         app_status = request.POST.get('app_status')
         applicant = request.POST.get('applicant')
         if app_status == 'Yes':
-            cur_app = EventApplication.objects.filter(event_name = event_id, user_who_applied = applicant).get()
+            cur_app = EventApplication.objects.get(event_name=event_id, user_who_applied=applicant)
             cur_app.status = True
             cur_app.save()
+            event_ref = Event.objects.get(id=event_id)
+            event_performer = cur_app.user_who_applied
+            performer = UserProfile.objects.get(user_id=event_performer.id)
+            performer_number = performer.phonenumber
+            payload = {'number': performer_number,
+                       'message': "Congratulations! You've been selected to perform at: " + event_ref.event_name +
+                                  ". Check out all your other applications to your events on PITCH!"}
+            requests.post('http://textbelt.com/text', data=payload)
         else:
             cur_app = EventApplication.objects.filter(event_name = event_id, user_who_applied = applicant).get()
             cur_app.status = False
             cur_app.save()
     return render(request, 'event_applicants.html', context)
+
 
 def show_applicants_ad(request, ad_id):
     ad = Musician_Advertisement.objects.filter(id = ad_id).get()
