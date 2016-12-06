2cd8b0c43f8928d11d81102ba3e6b6800346da18
bytesizedroll <damienhobday@gmail.com>
2016-12-06 02:19:07 -0500

    Static again, why is it me doing this.

--- gigfinder/settings.py
+++ gigfinder/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
@@ -30,6 +31,7 @@ STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(BASE_DIR, 'static')
 )
 
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'authentication/templates/')
