a3645d8ad2d25a6b9d30491316a7662924cdfc7b
Abhishek Samdaria <samdaria@bxscience.edu>
2016-12-08 11:12:03 -0500

    Added login, logout, logout tests

--- authentication/tests/test_views.py
+++ authentication/tests/test_views.py
@@ -1,7 +1,9 @@
 from django.core.urlresolvers import resolve
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
 from authentication.views import index
 from authentication.models import UserProfile
+from authentication.forms import UserForm, RegistrationForm
 from board.models import Event, Musician_Advertisement
 
 
@@ -9,3 +11,72 @@ class LandingPageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func, index)
+
+
+class AuthenticateTest(TestCase):
+    def setUp(self):
+        user_venue = User.objects.create(
+            username="test_venue_user",
+            email="test_venue@test.com",
+            password="password"
+        )
+
+        self.venue_profile = UserProfile(
+            user=user_venue,
+            type_user='Venue',
+            address='35W 67th st, New York, NY',
+            bio='Hello There',
+            website='www.google.com',
+            phonenumber='9999999999',
+            genre='Rock',
+            soundcloud_username='tfloud',
+        )
+
+        user_musician = User.objects.create(
+            username="test_musician_user",
+            email="test_musician@test.com",
+            password="password"
+        )
+
+        self.musician_profile = UserProfile(
+            user=user_musician,
+            type_user='Musician',
+            address='35W 67th st, New York, NY',
+            bio='Hello There',
+            website='www.google.com',
+            phonenumber='9999999999',
+            genre='Rock',
+            soundcloud_username='tfloud',
+        )
+
+        self.c = Client()
+
+    def test_login_venue(self):
+        response = self.c.get('/accounts/login/')
+        self.assertEqual(response.status_code, 200)
+        response = self.c.post('/accounts/login/', {
+            'username': 'test_venue_user',
+            'password': 'password'
+        })
+        self.assertEqual(response.status_code, 200)
+
+    def test_register_therapist(self):
+        response = self.c.get('/register/')
+        self.assertEqual(response.status_code, 200)
+
+        # Below test is commented due to bug in register view
+        # (MultiValueDictKeyError: "'type'").
+
+        response = self.c.post('/register/',
+                               {'username': 'test_venue_user',
+                                'email': 'test_venuer_user@test.com',
+                                'first_name': 'Philip',
+                                'last_name': 'Joseph',
+                                'password': 'password',
+                                }
+                               )
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout_view(self):
+        response = self.c.get('/logout/')
+        self.assertEqual(response.status_code, 200)
--- board/tests/test_models.py
+++ board/tests/test_models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from board.forms import *
 import unittest
 from board import models
-from board.models import EventApplication
+from board.models import EventApplication, AdApplication
 from datetime import date
 from django.utils import timezone
 from authentication.models import UserProfile
@@ -12,16 +12,16 @@ from authentication.models import UserProfile
 # testing models
 class EventTestCase(TestCase):
     def setUp(self):
-        user = User.objects.create(username='felipe', email='something@aol.com')
+        user = User(username='felipe', email='something@aol.com')
         self.profile = UserProfile(user=user,
-                                         type_user='Venue',
-                                         address='35W 67th st, New York, NY',
-                                         bio='Hello There',
-                                         website='www.google.com',
-                                         phonenumber='9999999999',
-                                         genre='Rock',
-                                         soundcloud_username='tfloud',
-                                         )
+                                   type_user='Venue',
+                                   address='35W 67th st, New York, NY',
+                                   bio='Hello There',
+                                   website='www.google.com',
+                                   phonenumber='9999999999',
+                                   genre='Rock',
+                                   soundcloud_username='tfloud',
+                                   )
 
         self.event1 = Event.objects.create(event_name="Event1",
                                            event_description_short="short description of the event",
@@ -31,7 +31,7 @@ class EventTestCase(TestCase):
 
         self.event_app = EventApplication(event_name=self.event1,
                                           user_who_applied=self.profile.user,
-                                         )
+                                          )
 
     def test_event(self):
         self.assertEquals(self.event1.event_name, 'Event1')
@@ -47,29 +47,35 @@ class EventTestCase(TestCase):
 
 class Musician_adTestCase(unittest.TestCase):
     def setUp(self):
-        user = User.objects.create(username='felipe', email='something@aol.com')
-        self.venue_profile = UserProfile(user=user,
-                                         type_user='Venue',
-                                         address='35W 67th st, New York, NY',
-                                         bio='Hello There',
-                                         website='www.google.com',
-                                         phonenumber='9999999999',
-                                         genre='Rock',
-                                         soundcloud_username='tfloud',
-                                         )
+        user2 = User(username='joe', email='something@gmail.com')
+        self.vprofile = UserProfile(user=user2,
+                                    type_user='Musician',
+                                    address='35W 67th st, New York, NY',
+                                    bio='Hello There',
+                                    website='www.google.com',
+                                    phonenumber='9999999999',
+                                    genre='Rock',
+                                    soundcloud_username='tfloud',
+                                    )
 
-        self.musicians_ads = Musician_Advertisement.objects.create(
-            musician_name=user,
-            ad_description_short='short ad description',
-            ad_description_long='long ad description',
-            start_availability=date(2017, 1, 1),
-            end_availability=date(2017, 1, 7)
-        )
+        self.ads1 = Musician_Advertisement.objects.create(posting_name='Hello',
+                                                          ad_description_short='short ad description',
+                                                          ad_description_long='long ad description',
+                                                          start_availability=date(2017, 1, 1),
+                                                          end_availability=date(2017, 1, 7)
+                                                          )
+
+        self.musician_app = AdApplication(ad_name=self.ads1,
+                                          user_who_applied=self.vprofile.user,
+                                          )
 
     def test_musician_ads(self):
-        self.assertEquals(self.musicians_ads.ad_description_short, 'short ad description')
-        self.assertEquals(self.musicians_ads.ad_description_long, 'long ad description')
-        self.assertEquals(self.musicians_ads.start_availability, date(2017, 1, 1))
-        self.assertEquals(self.musicians_ads.end_availability, date(2017, 1, 7))
-        self.assertEquals(self.musicians_ads.musician_name.username, 'felipe')
-        self.assertEquals(self.musicians_ads.__str__(), str(self.musicians_ads.posting_name))
+        self.assertEquals(self.ads1.ad_description_short, 'short ad description')
+        self.assertEquals(self.ads1.ad_description_long, 'long ad description')
+        self.assertEquals(self.ads1.start_availability, date(2017, 1, 1))
+        self.assertEquals(self.ads1.end_availability, date(2017, 1, 7))
+        self.assertEquals(self.ads1.__str__(), self.ads1.posting_name)
+
+    def test_ad_application(self):
+        self.assertEquals(self.musician_app.ad_name, self.ads1)
+        self.assertEquals(self.musician_app.__str__(), self.vprofile.user.username)
