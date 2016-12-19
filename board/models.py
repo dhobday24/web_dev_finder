e0382ac98601bb6908a79b32f64ea43c4140d188
Abhishek Samdaria <samdaria@bxscience.edu>
2016-12-19 17:08:31 -0500

    increased pylint score for board app

--- board/models.py
+++ board/models.py
@@ -1,17 +1,17 @@
 """
 Models for the board app
 """
-import datetime
 from django.db import models
-from django.utils import timezone
 from django.contrib.auth.models import User
-from django.core.exceptions import ValidationError
 
 from address.models import AddressField
 # Create your models here.
 
 
 class EventApplication(models.Model):
+    """
+    Model for all applications to attend an Event
+    """
     event_name = models.ForeignKey('Event', null=True)
     user_who_applied = models.ForeignKey(User)
     status = models.NullBooleanField(blank=True, default=None, null=True)
@@ -21,6 +21,9 @@ class EventApplication(models.Model):
 
 
 class AdApplication(models.Model):
+    """
+    Model for all Ad applications
+    """
     ad_name = models.ForeignKey('Musician_Advertisement', null=True)
     user_who_applied = models.ForeignKey(User)
     status = models.NullBooleanField(blank=True, default=None, null=True)
@@ -41,7 +44,7 @@ class Event(models.Model):
     event_time = models.TimeField(null=True)
     event_image = models.ImageField(null=True, blank=True)
     event_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    event_application = models.OneToOneField(EventApplication, null=True, blank = True)
+    event_application = models.OneToOneField(EventApplication, null=True, blank=True)
     event_address = AddressField(null=True)
     event_lat = models.DecimalField(null=True, decimal_places=3, max_digits=5)
     event_long = models.DecimalField(null=True, decimal_places=3, max_digits=5)
@@ -54,8 +57,9 @@ class Musician_Advertisement(models.Model):
     """
     Model for the advertisements for availability by musicians
     """
+    #pylint: disable=invalid-name
     posting_name = models.CharField(null=True, max_length=200)
-    musician_name = models.ForeignKey(User, on_delete = models.CASCADE, null=True)
+    musician_name = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     ad_description_short = models.CharField(max_length=400, blank=True)
     ad_description_long = models.CharField(max_length=2000)
     pub_date = models.DateTimeField(auto_now_add=True)
@@ -67,23 +71,3 @@ class Musician_Advertisement(models.Model):
     def __str__(self):
         string = str(self.posting_name)
         return string
-
-'''
-class Job_Posting(models.Model):
-    """
-    Model for the regular job postings
-    """
-    posting_name = models.CharField(max_length=200)
-    job_description_short = models.CharField(max_length=400, blank=True)
-    job_description_long = models.CharField(max_length=2000)
-    pub_date = models.DateTimeField(auto_now_add=True)
-    start_date = models.DateField()
-    end_date = models.DateField()
-    pay = models.IntegerField(default=0)
-    job_image = models.ImageField(null=True, blank=True)
-
-    def __str__(self):
-        return self.posting_name
-'''
-
-
