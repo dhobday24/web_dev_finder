2be470d218635e5d26618eff4487029918c3d285
bytesizedroll <damienhobday@gmail.com>
2016-12-19 15:50:40 -0500

    10/10 pylint for board/forms.py

--- board/forms.py
+++ board/forms.py
@@ -1,12 +1,10 @@
 """
 Board app forms based on models
 """
-# from django import forms
-from django.forms.widgets import DateInput
-from django.contrib.admin.widgets import AdminDateWidget
+
 import floppyforms.__future__ as forms
 from .models import Event, Musician_Advertisement
-from address.forms import AddressField
+
 
 #from board.models import Job_Posting
 
@@ -30,15 +28,3 @@ class AdForm(forms.ModelForm):
         model = Musician_Advertisement
         fields = '__all__'
         exclude = ['pub_date', 'musician_name', 'ad_application']
-
-
-'''
-class JobForm(forms.ModelForm):
-    """
-    Defines the form for the Job Postings model
-    """
-    class Meta:
-        model = Job_Posting
-        fields = '__all__'
-        exclude = []
-'''
